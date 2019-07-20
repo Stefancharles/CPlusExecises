dd5e7abfd5326475cba3cfa3de97570bc5502e73
Stefan <1437874755@qq.com>
2019-07-20 11:36:38 +0800

    add new

---
+++ cplustest/cmake-build-debug/CMakeFiles/cplustest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cplustest.dir/main.cpp.obj"
+  "cplustest.pdb"
+  "cplustest.exe"
+  "cplustest.exe.manifest"
+  "libcplustest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cplustest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test6/test6.cpp
@@ -0,0 +1,68 @@
+﻿#include<iostream>
+using namespace std;
+
+class Vehicle
+{
+public:
+	Vehicle();
+	~Vehicle();
+	void Run();
+	void Stop();
+private:
+	double MaxSpeed;
+	double Weight;
+};
+
+Vehicle::Vehicle()
+{
+}
+
+Vehicle::~Vehicle()
+{
+}
+
+void Vehicle::Run()
+{
+	cout << "Run" << endl;
+}
+
+void Vehicle::Stop()
+{
+	cout << "Stop" << endl;
+}
+
+class Bicycle :virtual public Vehicle
+{
+public:
+	Bicycle() {};
+	~Bicycle() {};
+	double Height;
+};
+
+class Motorcar :virtual public Vehicle
+{
+public:
+	Motorcar() {};
+	~Motorcar() {};
+	int SeatNum;
+};
+
+class Motorcycle :public Motorcar, public Bicycle
+{
+public:
+	Motorcycle() {};
+	void test();
+};
+
+void Motorcycle::test()
+{
+	cout << "Motorcycle..." << endl;
+}
+
+void main(void)
+{
+	Motorcycle test;
+	test.Run();
+	test.Stop();
+
+}
